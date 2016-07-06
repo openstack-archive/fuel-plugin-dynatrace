3b5748bdfeda9a5cfdafe359a53ea9396424555b
dirk <dirk@fueldev.dynatrace.vmta>
2016-07-06 10:16:31 +0200

    fixed hiera usage in deploy.pp

--- deployment_scripts/puppet/manifests/deploy.pp
+++ deployment_scripts/puppet/manifests/deploy.pp
@@ -1,5 +1,10 @@
 $dynatrace = hiera('dynatrace')
 
 if $dynatrace['enabled'] {
-  class {'ruxit_agent':}
+  class { 'ruxit_agent':
+    user          => 'root',
+    group         => 'root',
+    ruxit_tenant  => $dynatrace('ruxit_tenant'),
+    ruxit_token   => $dynatrace('ruxit_token'),
+  }
 }
