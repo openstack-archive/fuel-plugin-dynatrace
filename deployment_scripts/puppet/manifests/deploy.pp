7db188c541b5295c1754cfcecf4c21958580667b
dirkwall <dirk.wallerstorfer@ruxit.com>
2016-07-11 15:35:39 +0200

    fixed type of brackets

--- deployment_scripts/puppet/manifests/deploy.pp
+++ deployment_scripts/puppet/manifests/deploy.pp
@@ -4,7 +4,7 @@ if $dynatrace['enabled'] {
   class { 'ruxit_agent':
     user          => 'root',
     group         => 'root',
-    ruxit_tenant  => $dynatrace('dynatrace_tenant'),
-    ruxit_token   => $dynatrace('dynatrace_token'),
+    ruxit_tenant  => $dynatrace['dynatrace_tenant'],
+    ruxit_token   => $dynatrace['dynatrace_token'],
   }
 }
