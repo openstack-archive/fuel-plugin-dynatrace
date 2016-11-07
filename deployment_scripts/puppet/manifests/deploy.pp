c32ce1d570342ef8dbb3e0ed1f6f55a98e62555c
root <root@fuel.dirk.at>
2016-11-07 13:01:06 +0100

    revised puppet scripts with Georg

--- deployment_scripts/puppet/manifests/deploy.pp
+++
@@ -1,8 +0,0 @@
-$dynatrace = hiera('fuel-plugin-dynatrace')
-
-class { 'ruxit_agent':
-  user          => 'fueladmin',
-  group         => 'fueladmin',
-  ruxit_tenant  => $dynatrace['dynatrace_tenant'],
-  ruxit_token   => $dynatrace['dynatrace_token'],
-}
